4ab65499604e9c0c5d777b179f488ace2e3a8424
Wisdom Wemoatu Akanwe <151788806+Wiz1702@users.noreply.github.com>
2024-02-22 13:52:39 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,1 +1,14 @@
-
+<!-- Font Awesome icons -->
+<script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+<!-- Core theme JS -->
+<script src="js/scripts.js"></script>
+<script>
+    function shrinkImage() {
+        var image = document.getElementById('profilePicture');
+        image.classList.toggle('shrinked');
+    }
+</script>
+var myCollapse = document.getElementById('myCollapse')
+var bsCollapse = new bootstrap.Collapse(myCollapse, {
+  toggle: false
+})
